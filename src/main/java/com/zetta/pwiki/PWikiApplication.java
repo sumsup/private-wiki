991eaaa52e3e125b204a1a763a5832523a0ce4e6
KimMinSoo <starirof@naver.com>
2021-11-20 01:22:05 +0900

    Initial commit

---
+++ src/main/java/com/zetta/pwiki/PWikiApplication.java
@@ -0,0 +1,13 @@
+package com.zetta.pwiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PWikiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PWikiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zetta/pwiki/PWikiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zetta.pwiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PWikiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
